9890a5832c5269c219e54086738eb98324813323
Annie Hopper <anniehopper@mac.com>
2015-06-09 13:33:31 -0400

    oh nvm found it.
    the instagram is there but i want it up and no the side of the text container.
    
    the social media badge/links are just not showing up. idk if thats my fault or just the product of sketchy sourcing.

--- LoadComponents.js
+++ LoadComponents.js
@@ -1,3 +1,5 @@
 $("#header").load("header.html");
 $("#banner").load("banner.html");
+$("#instagram").load("instagram");
+$("social_media_links").load("social_media_links")
 $("#footer").load("footer.html");
