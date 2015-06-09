441ea80a4b0f8db70da966d07abc5f90c15a9892
Annie Hopper <anniehopper@mac.com>
2015-06-09 17:34:19 -0400

    like idk

--- LoadComponents.js
+++ LoadComponents.js
@@ -1,5 +1,5 @@
 $("#header").load("header.html");
 $("#banner").load("banner.html");
-$("nav_bar").load("nav_bar")
+$("#nav_bar").load("nav_bar.html")
 $("#media_bar").load("media_bar.html");
 $("#footer").load("footer.html");
