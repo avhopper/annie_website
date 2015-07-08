2f90bbcef43be75bc9f470b99b288650aadcd582
Sean Willerford <swillerford@icloud.com>
2015-07-08 09:10:01 -0400

    -Fleshed out some new pages -New buttons on sidebar -Worked on 2 different banners

--- LoadComponents.js
+++ LoadComponents.js
@@ -1,4 +1,4 @@
 $("#header").load("header.html");
-$("#banner").load("banner.html");
+$("#banner-large").load("banner.html");
 $("#media_bar").load("media_bar.html");
 $("#footer").load("footer.html");
