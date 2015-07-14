bf8259f1b5766a71b4ea38e6b03e7ee52d2bed0e
Annie Hopper <anniehopper@mac.com>
2015-07-14 11:45:09 -0400

    updated contact me page.

--- randomcat.js
+++ randomcat.js
@@ -6,6 +6,10 @@ cats[3] = 'cats/3.jpg';
 cats[4] = 'cats/4.jpg';
 cats[5] = 'cats/5.jpg';
 cats[6] = 'cats/6.jpg';
+cats[7] = 'cats/7.jpg';
+cats[8] = 'cats/8.jpg';
+cats[9] = 'cats/9.jpg';
+cats[10] = 'cats/10.jpg';
 
 var selectedCat = undefined;
 while (selectedCat == undefined){
